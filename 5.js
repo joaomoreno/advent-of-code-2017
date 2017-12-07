a3769cd5f1ecea0a405e8471f0f6cb686be2984d
Joao Moreno <mail@joaomoreno.com>
2017-12-07 22:42:26 +0100

    day 5 part 1

---
+++ 5.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+
+function stepsUntilExit(instructions) {
+	let steps = 0;
+	let pc = 0;
+
+	while (pc >= 0 && pc < instructions.length) {
+		const oldPc = pc;
+		pc += instructions[pc]++;
+		steps++;
+	}
+
+	return steps;
+}
+
+function main() {
+	const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+	const instructions = [];
+
+	rl.on('line', line => {
+		if (!line) {
+			process.exit(0);
+		}
+
+		instructions.push(parseInt(line));
+	});
+
+	rl.on('close', () => console.log(stepsUntilExit(instructions)));
+}
+
+main();
