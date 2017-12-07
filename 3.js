adc040cba56066e6b35f0f2bb985699dee62d31a
Joao Moreno <mail@joaomoreno.com>
2017-12-07 21:20:24 +0100

    day 3 part 2

--- 3.js
+++ 3.js
@@ -1,49 +1,105 @@
 const readline = require('readline');
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+
+function getCoord(index) {
+	let coord = [0, 0];
+	let max = 1;
+	let incr = 1;
+	let flip = false;
+	let direction = 0;
+
+	while (max < index) {
+		const diff = Math.min(index - max, incr);
+
+		switch (direction) {
+			case 0: coord[0] += diff; break;
+			case 1: coord[1] -= diff; break;
+			case 2: coord[0] -= diff; break;
+			case 3: coord[1] += diff; break;
+		}
+
+		max += diff;
+
+		if (max === index) {
+			return coord;
+		}
+
+		direction = (direction + 1) % 4;
+
+		if (flip) {
+			incr++;
+		}
+
+		flip = !flip;
+	}
+}
+
+function getFirstValueLargerThan(input) {
+	const grid = {};
+	grid['0,0'] = 1;
+	grid['1,0'] = 1;
+
+	const _ = (x, y) => grid[`${x},${y}`] || 0;
+
+	let x = 1;
+	let y = 1;
+	let direction = 'up';
+	let value;
+
+	while (true) {
+		value =
+			_(x - 1, y + 1) +
+			_(x - 1, y) +
+			_(x - 1, y - 1) +
+			_(x, y + 1) +
+			_(x, y - 1) +
+			_(x + 1, y + 1) +
+			_(x + 1, y) +
+			_(x + 1, y - 1);
+
+		if (value > input) {
+			return value;
+		}
+
+		grid[`${x},${y}`] = value;
+
+		if (x === y && x > 0) {
+			direction = 'left';
+		} else if (x === -y && x < 0) {
+			direction = 'down';
+		} else if (x === y && x < 0) {
+			direction = 'right';
+		} else if (x - 1 === - y && x > 0) {
+			direction = 'up';
+		}
+
+		switch (direction) {
+			case 'up': y++; break;
+			case 'left': x--; break;
+			case 'down': y--; break;
+			case 'right': x++; break;
+		}
+	}
+}
 
 function main() {
-	let result = 0;
-
-	rl.on('line', line => {
-		const n = parseInt(line);
-		let coord = [0, 0];
-		let max = 1;
-		let incr = 1;
-		let flip = false;
-		let direction = 0;
-
-		while (max < n) {
-			const diff = Math.min(n - max, incr);
-
-			switch (direction) {
-				case 0: coord[0] += diff; break;
-				case 1: coord[1] -= diff; break;
-				case 2: coord[0] -= diff; break;
-				case 3: coord[1] += diff; break;
-			}
-
-			max += diff;
-
-			if (max === n) {
-				console.log(coord);
-				console.log(Math.abs(coord[0]) + Math.abs(coord[1]));
-				return;
-			}
-
-			direction = (direction + 1) % 4;
-
-			if (flip) {
-				incr++;
-			}
-
-			flip = !flip;
-		}
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
 	});
 
-	rl.on('close', () => console.log(result));
+	rl.on('line', line => {
+		if (!line) {
+			process.exit(0);
+		}
+
+		const value = parseInt(line);
+		// const coord = getCoord(value);
+
+		// console.log(coord);
+		// console.log(Math.abs(coord[0]) + Math.abs(coord[1]));
+
+		console.log(getFirstValueLargerThan(value));
+	});
 }
 
 main();
